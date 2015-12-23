8d9ad0064fed6f4fca2db394f414f367da4b1663
Kanthi Nagaraj <kanthicn@stanford.edu>
2015-12-22 17:57:13 -0800

    dump all config before running
    Former-commit-id: 71cd57e08a4e3469b51f4d76850e2f700b0415f1
    Former-commit-id: 80ea07f61bbe22e954089d87cd308a6add7a23b2 [formerly f67025a42d9c21a986ec3cf0703cad6f4c3c2a19] [formerly 863b889ba6d04818e432a24600baccaed1ff8db6 [formerly f8ac170e97407ddb02b0785776b324a11127b67b]]
    Former-commit-id: acc523f9a2406cc4a5e4c6dc88f5d01119a8b1f0 [formerly 8fa333acdb1931603da7a59da216cad8fa4cc082]
    Former-commit-id: 547ae7c9d8d81d62ed2e723bc34035c9432681e3

---
+++ run_all_price_guard.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+import sys
+import os
+import subprocess
+from subprocess import Popen, PIPE
+
+arguments = {}
+
+if(len(sys.argv) < 2):
+  print("Usage : python run_config.py <executable> <config_file>")
+  sys.exit()
+plot_script=""
+f=open(sys.argv[2], 'r')
+for line in f:
+  if(len(line) < 3):
+    continue
+  str1=(line.strip()).split('=')
+  arg_key=str1[0]
+  if(arg_key[0] == '#'):
+    continue;
+  arg_val=str1[1]
+  if(arg_key == "plot_script"):
+    plot_script=arg_val
+    continue;
+  arguments[arg_key] = arg_val
+
+orig_prefix=arguments["prefix"]
+for pupdate_time in (0.000016, 0.000032, 0.000048, 0.000064):
+  for gupdate_time in (0.000016, 0.000032, 0.000048):
+    arguments["price_update_time"] = str(pupdate_time)
+    arguments["guardtime"] = str(gupdate_time)
+    prefix_str=orig_prefix
+    prefix_str=prefix_str+"_"+arguments["price_update_time"]+"_"+arguments["guardtime"]
+    arguments["prefix"] = prefix_str
+
+    final_args=""
+    for arg_key in arguments:
+      final_args = final_args+" --"+arg_key+"=\""+arguments[arg_key]+"\""
+
+    cmd_line="nohup ./waf --run \""+sys.argv[1]+final_args+"\""+" > "+prefix_str+".out "+" 2> "+prefix_str+".err &"
+    print(cmd_line)
+    subprocess.call(cmd_line, shell="False")
+
+#cmd_line="python "+plot_script+" "+prefix_str
+#print(cmd_line)
+#subprocess.call(cmd_line, shell="False")
+f.close()
+
