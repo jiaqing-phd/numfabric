2dd6281914ce381362245744ba52ec77ccbc5e61
Kanthi Nagaraj <kanthicn@stanford.edu>
2015-12-28 10:54:22 -0800

    SOme script changes
    Former-commit-id: 4ec60b00cf5ed7cc8aeca73e71b4f8f5cf546ed7
    Former-commit-id: 680b4af420debd2bb18e89555b1ad3091274cc57 [formerly 300c0299ef5abc2a62cf0013df4ce90e726775c9] [formerly 73d204abf13e08d13153e4cae1fd9f97fa22c7e9 [formerly 244f0370423019a1e1864f39fa37d728242086e7]]
    Former-commit-id: 986ab45b281465e02b89cf171eb1bb3073b909f5 [formerly b21c8bd60ba3143c9644ceae89008a75a75523e6]
    Former-commit-id: d126a7dcc46fa32a9858e4ebe48cfbec81df500d

--- run_all_price_guard.py
+++ run_all_price_guard.py
@@ -25,21 +25,24 @@ for line in f:
   arguments[arg_key] = arg_val
 
 orig_prefix=arguments["prefix"]
-for pupdate_time in (0.000016, 0.000032, 0.000048, 0.000064):
-  for gupdate_time in (0.000016, 0.000032, 0.000048):
-    arguments["price_update_time"] = str(pupdate_time)
-    arguments["guardtime"] = str(gupdate_time)
-    prefix_str=orig_prefix
-    prefix_str=prefix_str+"_"+arguments["price_update_time"]+"_"+arguments["guardtime"]
-    arguments["prefix"] = prefix_str
+for pupdate_time in (0.000016, 0.000020, 0.000040, 0.000060, 0.000080, 0.0001):
+  for gupdate_time in (0.0, 0.000001, 0.000008, 0.00001,0.00002, 0.00004):
+    for kval in (10000, 20000, 30000, 40000, 50000, 60000):
+      arguments["price_update_time"] = str(pupdate_time)
+      arguments["guardtime"] = str(gupdate_time)
+      arguments["kvalue_rate"] = str(kval)
+      arguments["kvalue_price"] = str(kval)
+      prefix_str=orig_prefix
+      prefix_str=prefix_str+"_"+arguments["price_update_time"]+"_"+arguments["guardtime"]+"_"+arguments["kvalue_price"]
+      arguments["prefix"] = prefix_str
 
-    final_args=""
-    for arg_key in arguments:
-      final_args = final_args+" --"+arg_key+"=\""+arguments[arg_key]+"\""
+      final_args=""
+      for arg_key in arguments:
+        final_args = final_args+" --"+arg_key+"=\""+arguments[arg_key]+"\""
 
-    cmd_line="nohup ./waf --run \""+sys.argv[1]+final_args+"\""+" > "+prefix_str+".out "+" 2> "+prefix_str+".err &"
-    print(cmd_line)
-    subprocess.call(cmd_line, shell="False")
+      cmd_line="nohup ./waf --run \""+sys.argv[1]+final_args+"\""+" > "+prefix_str+".out "+" 2> "+prefix_str+".err &"
+      print(cmd_line)
+      subprocess.call(cmd_line, shell="False")
 
 #cmd_line="python "+plot_script+" "+prefix_str
 #print(cmd_line)
