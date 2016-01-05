3b0590f460abd493dcb14516149b08827f468e46
Kanthi Nagaraj <kanthicn@stanford.edu>
2016-01-05 14:34:44 -0800

    plot_all_params together
    Former-commit-id: 5d74589624a624402ea3d35271117e14c5d34d09
    Former-commit-id: 6bf2c6073a3497e3f9532ddb385d168f791a1b79 [formerly 58a3c85d017a4602ea2ee4c9082ac0c4bbeaca67] [formerly 824405220fbf8c5c5eebd771e380b1a1fb4836c3 [formerly b80d8c31c39bae94564bda58f3960df64b5c2868]]
    Former-commit-id: fa0c337277f13fd33106feccb441fa86a9b49dd6 [formerly 320812981ed05c8973d9c26d07baf7fd6aec2cda]
    Former-commit-id: 3bc351f51760a7205634e4ab7ee6842333f850d2

---
+++ plot_all_params.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+import sys
+import os
+import subprocess
+from subprocess import Popen, PIPE
+
+arguments = {}
+
+if(len(sys.argv) < 2):
+  print("Usage : python run_config.py <executable> <config_file> <plot or not>")
+  sys.exit()
+
+plot_script="plot_rates.py"
+
+f=open(sys.argv[2], 'r')
+for line in f:
+  if(len(line) < 3):
+    continue
+  str1=(line.strip()).split('=')
+  arg_key=str1[0]
+  if(arg_key[0] == '#'):
+    continue;
+  arg_val=str1[1]
+  if(arg_key == "plot_script"):
+    plot_script=arg_val
+    continue;
+  arguments[arg_key] = arg_val
+
+#print(arguments)
+orig_prefix=arguments["prefix"]
+for pupdate_time in (0.000016, 0.000032,0.000048, 0.000064):
+  for gupdate_time in (0.0, 0.000016, 0.000032):
+    if(pupdate_time <= gupdate_time):
+        next
+    ewma_time=10000
+    for dt_val in (0.000012, 0.000024):
+        prefix_str=orig_prefix
+        prefix_str=prefix_str+"_"+arguments["price_update_time"]+"_"+arguments["guardtime"]+"_"+arguments["dt_val"]+"_"+arguments["kvalue_rate"]
+        # Let's first plot convergence CDF
+        converge_time_str = "python find_convergence_times.py "+prefix_str+".out mp 0.1 | grep maximum | cut -f2 -d\" \" >"+prefix_str+"_converge_times.out&"
+        print(converge_time_str)
+#        subprocess.call(converge_time_str, shell="False")
+f.close()
+
